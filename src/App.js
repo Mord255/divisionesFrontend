04198c91be3b14c49eea61e2203e78541d1e0a85
JeanpierreSolis <jean.p.ponte.solis@gmail.com>
2021-11-22 00:00:02 -0500

    cambios pier

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 import { DatePicker } from 'antd';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import BasicLayout from '@ant-design/pro-layout';
 
+import { Layout, Menu, Breadcrumb } from 'antd';
+
+const { Header, Content, Footer } = Layout;
 function App() {
     return (
-        <div className="App">
-            <br/><br/>
-            <DatePicker/>
-        </div>
+        <Layout className="layout">
+    <Header>
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        {new Array(15).fill(null).map((_, index) => {
+          const key = index + 1;
+          return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>;
+        })}
+      </Menu>
+    </Header>
+    <Content style={{ padding: '0 50px' }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+        <Breadcrumb.Item>App</Breadcrumb.Item>
+      </Breadcrumb>
+      <div className="site-layout-content">Content</div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+  </Layout>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import BasicLayout from '@ant-design/pro-layout';
 
 ReactDOM.render(
   <React.StrictMode>
